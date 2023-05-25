6ae639374b1b9081cc5f271a9ace6c06dc53f332
wangtuo0820 <wangtuo0820@outlook.com>
2023-05-25 14:00:57 +0800

    Create kitti_vis.py

---
+++ kitti_vis.py
@@ -0,0 +1,480 @@
+import os
+import sys
+import numpy as np
+import cv2
+import sys
+from tqdm import tqdm
+
+class kitti_object(object):
+    def __init__(self, root_dir, split="training", pred_dir="label_2"):
+        self.root_dir = root_dir
+        self.split = split
+        self.split_dir = os.path.join(root_dir, split)
+
+        self.image_dir = os.path.join(self.split_dir, "image_2")
+        self.gt_dir = os.path.join(self.split_dir, "label_2") 
+        self.label_dir = os.path.join(self.split_dir, pred_dir)
+        self.calib_dir = os.path.join(self.split_dir, "calib")
+
+    def get_image(self, idx):
+        img_filename = os.path.join(self.image_dir, "%06d.png" % (idx))
+        return cv2.imread(img_filename), os.path.abspath(img_filename)
+
+    def get_calibration(self, idx):
+        calib_filename = os.path.join(self.calib_dir, "%06d.txt" % (idx))
+        return Calibration(calib_filename)
+
+    def get_label_objects(self, idx):
+        label_filename = os.path.join(self.label_dir, "%06d.txt" % (idx))
+        lines = [line.rstrip() for line in open(label_filename)]
+        objects = [Object3d(line) for line in lines]
+        return objects
+
+    def get_gt_objects(self, idx):
+        label_filename = os.path.join(self.gt_dir, "%06d.txt" % (idx))
+        lines = [line.rstrip() for line in open(label_filename)]
+        objects = [Object3d(line) for line in lines]
+        return objects
+
+
+class Object3d(object):
+    """ 3d object label """
+
+    def __init__(self, label_file_line):
+        data = label_file_line.split(" ")
+        self.rawdata = data
+        data[1:] = [float(x) for x in data[1:]]
+
+        # extract label, truncation, occlusion
+        self.type = data[0]  # 'Car', 'Pedestrian', ...
+        self.truncation = data[1]  # truncated pixel ratio [0..1]
+        self.occlusion = int(
+            data[2]
+        )  # 0=visible, 1=partly occluded, 2=fully occluded, 3=unknown
+        self.alpha = data[3]  # object observation angle [-pi..pi]
+
+        # extract 2d bounding box in 0-based coordinates
+        self.xmin = data[4]  # left
+        self.ymin = data[5]  # top
+        self.xmax = data[6]  # right
+        self.ymax = data[7]  # bottom
+        self.box2d = np.array([self.xmin, self.ymin, self.xmax, self.ymax])
+
+        # extract 3d bounding box information
+        self.h = data[8]  # box height
+        self.w = data[9]  # box width
+        self.l = data[10]  # box length (in meters)
+        self.t = (data[11], data[12], data[13])  # location (x,y,z) in camera coord.
+        # self.t = (data[11], data[13]+self.h, data[12])  # location (x,y,z) in camera coord. TODO
+        self.ry = data[14]  # yaw angle (around Y-axis in camera coordinates) [-pi..pi]
+
+    def __str__(self):
+        return "Object3d:\n" + str(self.rawdata)
+
+    def estimate_diffculty(self):
+        """ Function that estimate difficulty to detect the object as defined in kitti website"""
+        # height of the bounding box
+        bb_height = np.abs(self.xmax - self.xmin)
+
+        if bb_height >= 40 and self.occlusion == 0 and self.truncation <= 0.15:
+            return "Easy"
+        elif bb_height >= 25 and self.occlusion in [0, 1] and self.truncation <= 0.30:
+            return "Moderate"
+        elif (
+            bb_height >= 25 and self.occlusion in [0, 1, 2] and self.truncation <= 0.50
+        ):
+            return "Hard"
+        else:
+            return "Unknown"
+
+    def print_object(self):
+        print(
+            "Type, truncation, occlusion, alpha: %s, %d, %d, %f"
+            % (self.type, self.truncation, self.occlusion, self.alpha)
+        )
+        print(
+            "2d bbox (x0,y0,x1,y1): %f, %f, %f, %f"
+            % (self.xmin, self.ymin, self.xmax, self.ymax)
+        )
+        print("3d bbox h,w,l: %f, %f, %f" % (self.h, self.w, self.l))
+        print(
+            "3d bbox location, ry: (%f, %f, %f), %f"
+            % (self.t[0], self.t[1], self.t[2], self.ry)
+        )
+        print("Difficulty of estimation: {}".format(self.estimate_diffculty()))
+
+
+class Calibration(object):
+    """ Calibration matrices and utils
+        3d XYZ in <label>.txt are in rect camera coord.
+        2d box xy are in image2 coord
+        Points in <lidar>.bin are in Velodyne coord.
+
+        y_image2 = P^2_rect * x_rect
+        y_image2 = P^2_rect * R0_rect * Tr_velo_to_cam * x_velo
+        x_ref = Tr_velo_to_cam * x_velo
+        x_rect = R0_rect * x_ref
+
+        P^2_rect = [f^2_u,  0,      c^2_u,  -f^2_u b^2_x;
+                    0,      f^2_v,  c^2_v,  -f^2_v b^2_y;
+                    0,      0,      1,      0]
+                 = K * [1|t]
+
+        image2 coord:
+         ----> x-axis (u)
+        |
+        |
+        v y-axis (v)
+
+        velodyne coord:
+        front x, left y, up z
+
+        rect/ref camera coord:
+        right x, down y, front z
+
+        Ref (KITTI paper): http://www.cvlibs.net/publications/Geiger2013IJRR.pdf
+
+        TODO(rqi): do matrix multiplication only once for each projection.
+    """
+
+    def inverse_rigid_trans(self, Tr):
+        """ Inverse a rigid body transform matrix (3x4 as [R|t])
+            [R'|-R't; 0|1]
+        """
+        inv_Tr = np.zeros_like(Tr)  # 3x4
+        inv_Tr[0:3, 0:3] = np.transpose(Tr[0:3, 0:3])
+        inv_Tr[0:3, 3] = np.dot(-np.transpose(Tr[0:3, 0:3]), Tr[0:3, 3])
+        return inv_Tr
+
+
+    def __init__(self, calib_filepath, from_video=False):
+        if from_video:
+            calibs = self.read_calib_from_video(calib_filepath)
+        else:
+            calibs = self.read_calib_file(calib_filepath)
+        # Projection matrix from rect camera coord to image2 coord
+        self.P = calibs["P2"]
+        self.P = np.reshape(self.P, [3, 4])
+        # Rigid transform from Velodyne coord to reference camera coord
+        self.V2C = calibs["Tr_velo_to_cam"]
+        self.V2C = np.reshape(self.V2C, [3, 4])
+        self.C2V = self.inverse_rigid_trans(self.V2C)
+        # Rotation from reference camera coord to rect camera coord
+        self.R0 = calibs["R0_rect"]
+        self.R0 = np.reshape(self.R0, [3, 3])
+
+        # Camera intrinsics and extrinsics
+        self.c_u = self.P[0, 2]
+        self.c_v = self.P[1, 2]
+        self.f_u = self.P[0, 0]
+        self.f_v = self.P[1, 1]
+        self.b_x = self.P[0, 3] / (-self.f_u)  # relative
+        self.b_y = self.P[1, 3] / (-self.f_v)
+
+    def __str__(self):
+        return "calib: \n" + str(self.P)
+
+    def read_calib_file(self, filepath):
+        """ Read in a calibration file and parse into a dictionary.
+        Ref: https://github.com/utiasSTARS/pykitti/blob/master/pykitti/utils.py
+        """
+        data = {}
+        with open(filepath, "r") as f:
+            for line in f.readlines():
+                line = line.rstrip()
+                if len(line) == 0:
+                    continue
+                key, value = line.split(":", 1)
+                # The only non-float values in these files are dates, which
+                # we don't care about anyway
+                try:
+                    data[key] = np.array([float(x) for x in value.split()])
+                except ValueError:
+                    pass
+
+        return data
+
+    def read_calib_from_video(self, calib_root_dir):
+        """ Read calibration for camera 2 from video calib files.
+            there are calib_cam_to_cam and calib_velo_to_cam under the calib_root_dir
+        """
+        data = {}
+        cam2cam = self.read_calib_file(
+            os.path.join(calib_root_dir, "calib_cam_to_cam.txt")
+        )
+        velo2cam = self.read_calib_file(
+            os.path.join(calib_root_dir, "calib_velo_to_cam.txt")
+        )
+        Tr_velo_to_cam = np.zeros((3, 4))
+        Tr_velo_to_cam[0:3, 0:3] = np.reshape(velo2cam["R"], [3, 3])
+        Tr_velo_to_cam[:, 3] = velo2cam["T"]
+        data["Tr_velo_to_cam"] = np.reshape(Tr_velo_to_cam, [12])
+        data["R0_rect"] = cam2cam["R_rect_00"]
+        data["P2"] = cam2cam["P_rect_02"]
+        return data
+
+    def cart2hom(self, pts_3d):
+        """ Input: nx3 points in Cartesian
+            Oupput: nx4 points in Homogeneous by pending 1
+        """
+        n = pts_3d.shape[0]
+        pts_3d_hom = np.hstack((pts_3d, np.ones((n, 1))))
+        return pts_3d_hom
+
+    # ===========================
+    # ------- 3d to 3d ----------
+    # ===========================
+    def project_velo_to_ref(self, pts_3d_velo):
+        pts_3d_velo = self.cart2hom(pts_3d_velo)  # nx4
+        return np.dot(pts_3d_velo, np.transpose(self.V2C))
+
+    def project_ref_to_velo(self, pts_3d_ref):
+        pts_3d_ref = self.cart2hom(pts_3d_ref)  # nx4
+        return np.dot(pts_3d_ref, np.transpose(self.C2V))
+
+    def project_rect_to_ref(self, pts_3d_rect):
+        """ Input and Output are nx3 points """
+        return np.transpose(np.dot(np.linalg.inv(self.R0), np.transpose(pts_3d_rect)))
+
+    def project_ref_to_rect(self, pts_3d_ref):
+        """ Input and Output are nx3 points """
+        return np.transpose(np.dot(self.R0, np.transpose(pts_3d_ref)))
+
+    def project_rect_to_velo(self, pts_3d_rect):
+        """ Input: nx3 points in rect camera coord.
+            Output: nx3 points in velodyne coord.
+        """
+        pts_3d_ref = self.project_rect_to_ref(pts_3d_rect)
+        return self.project_ref_to_velo(pts_3d_ref)
+
+    def project_velo_to_rect(self, pts_3d_velo):
+        pts_3d_ref = self.project_velo_to_ref(pts_3d_velo)
+        return self.project_ref_to_rect(pts_3d_ref)
+
+    # ===========================
+    # ------- 3d to 2d ----------
+    # ===========================
+    def project_rect_to_image(self, pts_3d_rect):
+        """ Input: nx3 points in rect camera coord.
+            Output: nx2 points in image2 coord.
+        """
+        pts_3d_rect = self.cart2hom(pts_3d_rect)
+        pts_2d = np.dot(pts_3d_rect, np.transpose(self.P))  # nx3
+        pts_2d[:, 0] /= pts_2d[:, 2]
+        pts_2d[:, 1] /= pts_2d[:, 2]
+        return pts_2d[:, 0:2]
+
+    def project_velo_to_image(self, pts_3d_velo):
+        """ Input: nx3 points in velodyne coord.
+            Output: nx2 points in image2 coord.
+        """
+        pts_3d_rect = self.project_velo_to_rect(pts_3d_velo)
+        return self.project_rect_to_image(pts_3d_rect)
+
+    def project_8p_to_4p(self, pts_2d):
+        x0 = np.min(pts_2d[:, 0])
+        x1 = np.max(pts_2d[:, 0])
+        y0 = np.min(pts_2d[:, 1])
+        y1 = np.max(pts_2d[:, 1])
+        x0 = max(0, x0)
+        # x1 = min(x1, proj.image_width)
+        y0 = max(0, y0)
+        # y1 = min(y1, proj.image_height)
+        return np.array([x0, y0, x1, y1])
+
+    def project_velo_to_4p(self, pts_3d_velo):
+        """ Input: nx3 points in velodyne coord.
+            Output: 4 points in image2 coord.
+        """
+        pts_2d_velo = self.project_velo_to_image(pts_3d_velo)
+        return self.project_8p_to_4p(pts_2d_velo)
+
+    # ===========================
+    # ------- 2d to 3d ----------
+    # ===========================
+    def project_image_to_rect(self, uv_depth):
+        """ Input: nx3 first two channels are uv, 3rd channel
+                   is depth in rect camera coord.
+            Output: nx3 points in rect camera coord.
+        """
+        n = uv_depth.shape[0]
+        x = ((uv_depth[:, 0] - self.c_u) * uv_depth[:, 2]) / self.f_u + self.b_x
+        y = ((uv_depth[:, 1] - self.c_v) * uv_depth[:, 2]) / self.f_v + self.b_y
+        pts_3d_rect = np.zeros((n, 3))
+        pts_3d_rect[:, 0] = x
+        pts_3d_rect[:, 1] = y
+        pts_3d_rect[:, 2] = uv_depth[:, 2]
+        return pts_3d_rect
+
+    def project_image_to_velo(self, uv_depth):
+        pts_3d_rect = self.project_image_to_rect(uv_depth)
+        return self.project_rect_to_velo(pts_3d_rect)
+
+    def project_depth_to_velo(self, depth, constraint_box=True):
+        depth_pt3d = get_depth_pt3d(depth)
+        depth_UVDepth = np.zeros_like(depth_pt3d)
+        depth_UVDepth[:, 0] = depth_pt3d[:, 1]
+        depth_UVDepth[:, 1] = depth_pt3d[:, 0]
+        depth_UVDepth[:, 2] = depth_pt3d[:, 2]
+        # print("depth_pt3d:",depth_UVDepth.shape)
+        depth_pc_velo = self.project_image_to_velo(depth_UVDepth)
+        # print("dep_pc_velo:",depth_pc_velo.shape)
+        if constraint_box:
+            depth_box_fov_inds = (
+                (depth_pc_velo[:, 0] < cbox[0][1])
+                & (depth_pc_velo[:, 0] >= cbox[0][0])
+                & (depth_pc_velo[:, 1] < cbox[1][1])
+                & (depth_pc_velo[:, 1] >= cbox[1][0])
+                & (depth_pc_velo[:, 2] < cbox[2][1])
+                & (depth_pc_velo[:, 2] >= cbox[2][0])
+            )
+            depth_pc_velo = depth_pc_velo[depth_box_fov_inds]
+        return depth_pc_velo
+
+
+def compute_box_3d(obj, P):
+    """ Takes an object and a projection matrix (P) and projects the 3d
+        bounding box into the image plane.
+        Returns:
+            corners_2d: (8,2) array in left image coord.
+            corners_3d: (8,3) array in in rect camera coord.
+    """
+    def roty(t):
+        c = np.cos(t)
+        s = np.sin(t)
+        return np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
+
+    # compute rotational matrix around yaw axis
+    R = roty(obj.ry)
+
+    # 3d bounding box dimensions
+    l = obj.l
+    w = obj.w
+    h = obj.h
+
+
+    # 3d bounding box corners
+    x_corners = [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2]
+    y_corners = [0, 0, 0, 0, -h, -h, -h, -h]
+    z_corners = [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2]
+
+    # rotate and translate 3d bounding box
+    corners_3d = np.dot(R, np.vstack([x_corners, y_corners, z_corners]))
+    # print corners_3d.shape
+    corners_3d[0, :] = corners_3d[0, :] + obj.t[0]
+    corners_3d[1, :] = corners_3d[1, :] + obj.t[1]
+    corners_3d[2, :] = corners_3d[2, :] + obj.t[2]
+
+    # print 'cornsers_3d: ', corners_3d
+    # only draw 3d bounding box for objs in front of the camera
+    if np.any(corners_3d[2, :] < 0.1):
+        corners_2d = None
+        return corners_2d, np.transpose(corners_3d)
+
+    # project the 3d bounding box into the image plane
+    corners_2d = project_to_image(np.transpose(corners_3d), P)
+    # print 'corners_2d: ', corners_2d
+    return corners_2d, np.transpose(corners_3d)
+
+def project_to_image(pts_3d, P):
+    """ Project 3d points to image plane.
+
+    Usage: pts_2d = projectToImage(pts_3d, P)
+      input: pts_3d: nx3 matrix
+             P:      3x4 projection matrix
+      output: pts_2d: nx2 matrix
+
+      P(3x4) dot pts_3d_extended(4xn) = projected_pts_2d(3xn)
+      => normalize projected_pts_2d(2xn)
+
+      <=> pts_3d_extended(nx4) dot P'(4x3) = projected_pts_2d(nx3)
+          => normalize projected_pts_2d(nx2)
+    """
+    n = pts_3d.shape[0]
+    pts_3d_extend = np.hstack((pts_3d, np.ones((n, 1))))
+    # print(('pts_3d_extend shape: ', pts_3d_extend.shape))
+    pts_2d = np.dot(pts_3d_extend, np.transpose(P))  # nx3
+    pts_2d[:, 0] /= pts_2d[:, 2]
+    pts_2d[:, 1] /= pts_2d[:, 2]
+    return pts_2d[:, 0:2]
+
+
+
+def show_image_with_boxes(img, objects, calib, is_gt=False):
+    img_3d = np.copy(img)
+    color_dict = {"Car": (255,0,0), "Pedestrian": (255,255,0), "Cyclist": (255,0,255), "Truck": (255,0,0), "Misc": (0,0,255), "Van": (0,0,255), "Tram": (0,0,255), "Person_sitting": (255,255,255)}
+
+    for obj in objects:
+        if obj.type == "DontCare":
+            continue
+        # draw 2d box
+        # cv2.rectangle(img_3d, 
+        #             (int(obj.xmin), int(obj.ymin)), (int(obj.xmax), int(obj.xmax))
+        #             color_dict[obj.type], 2)
+        box3d_pts_2d, _ = compute_box_3d(obj, calib.P)
+
+        if type(box3d_pts_2d) == type(None):
+            print("only draw 3d bounding box for objs in front of the camera")
+            continue
+        img_3d = draw_projected_box3d(img_3d, box3d_pts_2d, color=(0,0,255) if is_gt else color_dict[obj.type])
+
+    return img_3d
+
+def draw_projected_box3d(image, qs, color=(0, 255, 0), thickness=2):
+    """ Draw 3d bounding box in image
+        qs: (8,3) array of vertices for the 3d box in following order:
+            1 -------- 0
+           /|         /| 
+          2 -------- 3 .
+          | |        | |
+          . 5 -------- 4
+          |/         |/
+          6 -------- 7
+    """ 
+    qs = qs.astype(np.int32) 
+    for k in range(0, 4):
+        # Ref: http://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html
+        i, j = k, (k + 1) % 4
+        # use LINE_AA for opencv3
+        # cv2.line(image, (qs[i,0],qs[i,1]), (qs[j,0],qs[j,1]), color, thickness, cv2.CV_AA)
+        cv2.line(image, (qs[i, 0], qs[i, 1]), (qs[j, 0], qs[j, 1]), color, thickness)
+        i, j = k + 4, (k + 1) % 4 + 4
+        cv2.line(image, (qs[i, 0], qs[i, 1]), (qs[j, 0], qs[j, 1]), color, thickness)
+
+        i, j = k, k + 4
+        cv2.line(image, (qs[i, 0], qs[i, 1]), (qs[j, 0], qs[j, 1]), color, thickness)
+    return image
+
+
+
+if __name__ == "__main__":
+    root_dir = "/your_path_to_kitti" # TODO
+
+    if(len(sys.argv) > 1): # your prediction path TODO 
+        #data_idxs = [i for i in range(int(sys.argv[1]), 1000)]
+        pred_dir = sys.argv[1]
+    else:
+        #data_idxs = [i for i in range(1000)]
+        pred_dir = "label_2"
+    
+    with open(root_dir+"/training/ImageSets/test.txt") as f:
+        data_idxs = f.readlines()
+
+    data_idxs = [int(item) for item in data_idxs]
+
+    for data_idx in tqdm(data_idxs):
+        dataset = kitti_object(root_dir, split="training", pred_dir=pred_dir)
+
+        img, img_path = dataset.get_image(data_idx)
+        objects = dataset.get_label_objects(data_idx)
+        objects_gt = dataset.get_gt_objects(data_idx)
+        calib = dataset.get_calibration(data_idx)
+
+        img_3d = show_image_with_boxes(img, objects, calib)
+        img_3d = show_image_with_boxes(img_3d, objects_gt, calib, is_gt=True)
+
+        cv2.imwrite(img_path.replace(".png", "_vis3d.png"), img_3d)
+        print("Save result to: " + img_path.replace(".png", "_vis3d.png"))
+        
+        cv2.imshow("3dbox", img_3d)
+        cv2.waitKey()
